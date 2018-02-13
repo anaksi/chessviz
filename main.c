546f7e841c11cf19e7616d40349509877e1bc1d8
Кирюхин Андрей Алексеевич <ip715s09@mwp01.csc.local>
2018-02-13 15:07:14 +0700

    Change chess board

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-printf(" R K B K Q B K R\n P P P P P P P P \n \n \n \n \n P P P P P P P P \n R K B K Q B K R \n");
+ printf(" \
+  R K B K Q B K R \n \
+  P P P P P P P P \n \
+                  \n \
+                  \n \
+                  \n \
+                  \n \
+  P P P P P P P P \n \
+  R K B K Q B K R \n");
 
 
 
